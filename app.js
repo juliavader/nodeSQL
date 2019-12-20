d6c5bde3e93f5e6a0f87c87a53e65c3d5ce9ce0a
juliavader <juliavader.contact@gmail.com>
2019-12-20 14:04:25 +0100

    adding connection to local sql

--- app.js
+++ app.js
@@ -2,8 +2,36 @@
 const express = require('express');
 const app = express();
 const morgan = require("morgan");
+const mysql = require('mysql')
 
 app.use(morgan('combined'));
+
+app.get('/user/:id', (req, res)=>{
+    console.log("fetching user with id : " + req.params.id)
+    const connection = mysql.createConnection({
+    host : '127.0.0.1',
+    port : 8889,
+    user     : 'root',
+    password : 'root',
+    database : 'sinterklaas'
+    })
+
+    const queryString = "SELECT * FROM utilisateur  WHERE id= ?"
+    connection.query( queryString,[req.params.id], (err, rows, fields)=>{
+        if(err){
+            console.log('failed to query for user '+ err);
+            res.sendStatus(500);
+            res.end();
+        }
+        const users = rows.map((row)=>{
+            return {firstName : row.prenom, lastname : row.nom}
+        })
+        res.json(users)
+        
+    })
+
+})
+
 app.get('/',(req, res)=>{
     console.log('responding to route ');
     res.send('hello from route ')
